fd4df36cc025716b3f6056bb2f8a9339239bc365
nkinnacostly <ayangcostly@gmail.com>
2025-04-24 16:29:51 +0100

    fixes

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -179,7 +179,7 @@ function Dashboard() {
             </div>
           </div>
         </div> */}
-        <LiveClasses meetings={liveClasses.meetings} />
+        <LiveClasses meetings={liveClasses?.meetings} />
       </div>
       <DashboardChallenges />
     </>
